3c100848be550858097bbb61d5f959ae0d8aaf28
Daniel Baker <dbaker@vistaprint.com>
2017-12-12 11:58:27 +0100

    Adding default variable descriptions

--- main.tf
+++ main.tf
@@ -1,20 +1,25 @@
 variable "default_threshold" {
-  default = 80
+  description = "The default threshold for the metric."
+  default = 5
 }
 
 variable "default_evaluation_periods" {
+  description = "The default amount of evaluation periods."
   default = 2
 }
 
 variable "default_period" {
+  description = "The default evaluation period."
   default = 60
 }
 
 variable "default_comparison_operator" {
+  description = "The default comparison operator."
   default = "GreaterThanOrEqualToThreshold"
 }
 
 variable "default_statistic" {
+  description = "The default statistic."
   default = "Average"
 }
 
